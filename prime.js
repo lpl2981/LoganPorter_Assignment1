c1e7d3f56100a3271b9f1396d3094a7f7a26935a
Porter <lporter@netsuite.com>
2018-01-15 17:34:45 -0600

    Cleaned up function

--- prime.js
+++ prime.js
@@ -1,8 +1,10 @@
+prime = 2;
+pCount = 1;
 var primeChecker = function(x) {
     if (x < 2)
         return "Not a Prime";
-    for (var count = 0; count < prime; count++) {
-        if (count % x === 0)
+    for (pCount; pCount < prime; pCount++) {
+        if (pCount % x === 0)
             return x;
         else return "Not a Prime";
     }
