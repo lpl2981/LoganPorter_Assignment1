05099bdfcec32c99dce47a5c92cbc80d3dd9489d
Porter <lporter@netsuite.com>
2018-01-15 17:43:43 -0600

    Fixed variable (reversed them in function)
    Realized I had the variables reversed in the function which is what was what was returning all numbers as prime

--- prime.js
+++ prime.js
@@ -1,12 +1,11 @@
-prime = 2;
-pCount = 1;
+pCount = 2;
 var primeChecker = function(x) {
     if (x < 2)
         return "Not a Prime";
-    for (pCount; pCount < prime; pCount++) {
-        if (pCount % x === 0)
-            return x;
-        else return "Not a Prime";
+    for (pCount; pCount < x; pCount++) {
+        if (x % pCount === 0)
+            return "Not a prime";
     }
+    return x;
 };
-console.log(primeChecker(6));
+console.log(primeChecker(5));
