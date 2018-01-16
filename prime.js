727da15921dc1bf46036790875609c861a4afcf0
Porter <lporter@netsuite.com>
2018-01-15 23:01:20 -0600

    Change to loop

--- prime.js
+++ prime.js
@@ -1,15 +1,18 @@
 var pCount = 2;
 var primeChecker = function(x) {
     if (x < 2)
-        return "Not a Prime";
+        return false;
     for (pCount; pCount < x; pCount++) {
         if (x % pCount === 0)
-            return "Not a Prime";
+            return false;
     }
     return x;
 };
-console.log(primeChecker(5));
-for (pCount = 0; pCount = 1000; pCount++);
-    if (primeChecker(pCount++) !== "Not a Prime")
-        var prime = true;
+for (pCount = 0; pCount > 1000; pCount++) {
+    if (primeChecker !== false) {
+        console.log(pCount);
+    }
+        break;
+}
+
 
