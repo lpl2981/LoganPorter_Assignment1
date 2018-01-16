e0f1ef2c2d81ebdf234432981342a9c393e3af31
Porter <lporter@netsuite.com>
2018-01-15 18:00:35 -0600

    Fix to variable name

--- prime.js
+++ prime.js
@@ -1,11 +1,11 @@
-pCount = 2;
+var pCount = 2;
 var primeChecker = function(x) {
     if (x < 2)
         return "Not a Prime";
     for (pCount; pCount < x; pCount++) {
         if (x % pCount === 0)
-            return "Not a prime";
+            return "Not a Prime";
     }
     return x;
 };
-console.log(primeChecker(5));
+console.log(primeChecker(4));
