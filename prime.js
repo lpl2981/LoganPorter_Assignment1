84a6833bed49011801508a1a9a37dbc5459bf57e
Porter <lporter@netsuite.com>
2018-01-16 15:01:37 -0600

    Corrected loop from For to While loop, various corrections to calculation

--- prime.js
+++ prime.js
@@ -1,18 +1,27 @@
-var pCount = 2;
-var primeChecker = function(x) {
+function primeChecker(x) {
     if (x < 2)
         return false;
-    for (pCount; pCount < x; pCount++) {
+    for (var pCount = 2; pCount < x; pCount++) {
         if (x % pCount === 0)
             return false;
     }
     return x;
 };
-for (pCount = 0; pCount > 1000; pCount++) {
-    if (primeChecker !== false) {
+console.log(primeChecker(350));
+/**
+ First attempt with for loop, attempted 2nd time with while loop to get correct result :)
+
+    for (var pCount = 0; pCount < 1000; pCount++) {
+    if (primeChecker(pCount)) {
         console.log(pCount);
-    }
-        break;
 }
-
-
+} **/
+var pCount = 0;
+var counter = 0;
+while (pCount < 1000) {
+    counter += 1;
+    if (primeChecker(counter)) {
+        pCount += 1;
+    }
+}
+console.log(counter);
